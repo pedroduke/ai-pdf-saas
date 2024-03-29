603b1ce27bd9a30a8dfa5cf90d362c10e5d1d03e
Pedro Duque <pedroduque8@gmail.com>
2024-03-29 18:27:53 +0100

    instant feedback from message

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -98,6 +98,87 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
         previousMessages: previousMessages?.pages.flatMap((page) => page.messages) ?? [],
       };
     },
+    onSuccess: async (stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description: 'Please refresh this page and try again',
+          variant: 'destructive',
+        });
+      }
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      // accumulated response
+      let accResponse = '';
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+
+        accResponse += chunkValue;
+
+        // append chunk to the actual message
+        utils.getFileMessages.setInfiniteData({ fileId, limit: INFINITE_QUERY_LIMIT }, (old) => {
+          if (!old) return { pages: [], pageParams: [] };
+
+          let isAiResponseCreated = old.pages.some((page) =>
+            page.messages.some((message) => message.id === 'ai-response'),
+          );
+
+          let updatedPages = old.pages.map((page) => {
+            if (page === old.pages[0]) {
+              let updatedMessages;
+
+              if (!isAiResponseCreated) {
+                updatedMessages = [
+                  {
+                    createdAt: new Date().toISOString(),
+                    id: 'ai-response',
+                    text: accResponse,
+                    isUserMessage: false,
+                  },
+                  ...page.messages,
+                ];
+              } else {
+                updatedMessages = page.messages.map((message) => {
+                  if (message.id === 'ai-response') {
+                    return {
+                      ...message,
+                      text: accResponse,
+                    };
+                  }
+                  return message;
+                });
+              }
+
+              return {
+                ...page,
+                messages: updatedMessages,
+              };
+            }
+
+            return page;
+          });
+
+          return { ...old, pages: updatedPages };
+        });
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current);
+      utils.getFileMessages.setData({ fileId }, { messages: context?.previousMessages ?? [] });
+    },
+    onSettled: async () => {
+      setIsLoading(false);
+
+      await utils.getFileMessages.invalidate({ fileId }); // ?????
+    },
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
