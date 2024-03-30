ecf57bb46039db0e9b34b046e1f93c2e4932ccc1
Pedro Duque <pedroduque8@gmail.com>
2024-03-30 12:49:10 +0100

    add suspense

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -28,15 +28,14 @@ const Page = () => {
   });
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-zinc-800" />}>
           <h3 className="font-semibold text-xl">Setting up your account...</h3>
           <p>You will be redirected automatically.</p>
-        </div>
+        </Suspense>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
