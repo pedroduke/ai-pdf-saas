740676249127deb258f09f45a3f567424e74dee5
Pedro Duque <pedroduque8@gmail.com>
2024-04-01 11:30:22 +0200

    check redirect changes

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,37 +3,53 @@
 import { trpc } from '@/app/_trpc/client';
 import { Loader2 } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { useRef } from 'react';
+
+// import { Suspense } from 'react';
 
 const Page = () => {
+  const retry = useRef(0);
+  const maxRetryCount = 10;
+
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
 
-  trpc.authCallback.useQuery(undefined, {
+  const { refetch } = trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
+      console.log(success);
       if (success) {
         // user is synced to db
         router.push(origin ? `/${origin}` : '/dashboard');
       }
     },
     onError: (err) => {
+      console.log(err);
       if (err.data?.code === 'UNAUTHORIZED') {
-        router.push('/sign-in');
+        retry.current = retry.current + 1;
+        if (retry.current <= maxRetryCount) {
+          // Retry up to maxRetryCount
+          setTimeout(() => {
+            refetch();
+          }, 500);
+        } else {
+          router.push('/sign-in');
+        }
       }
     },
-    retry: true,
+    retry: false,
     retryDelay: 500,
   });
 
   return (
     <div className="w-full mt-24 flex justify-center">
       <div className="flex flex-col items-center gap-2">
-        <Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-zinc-800" />}>
-          <h3 className="font-semibold text-xl">Setting up your account...</h3>
-          <p>You will be redirected automatically.</p>
-        </Suspense>
+        {/* <Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-zinc-800" />}> */}
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+        {/* </Suspense> */}
       </div>
     </div>
   );
